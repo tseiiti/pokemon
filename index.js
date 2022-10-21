f0686f7fd80981f412b49bc99a2224414e884c6e
Oscar Sei Iti Taniguchi <tseiiti@gmail.com>
2022-10-21 16:02:49 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-<h1>Hello, component!</h1>
-<p>teste de componente</p>
+loadComponents();
+
+var path = window.location.pathname;
+var page = path.split("/").pop();
+console.log( page );
+console.log( page.split(".").pop() );
