cf29ef8e29ffce9d5c472635adce98598f8876d3
Oscar Sei Iti Taniguchi <tseiiti@gmail.com>
2022-10-21 18:36:00 -0300

    index_component justify-content-cente

--- exports.js
+++ exports.js
@@ -1,15 +1,13 @@
-// querySelectorAll
-function qsa(arg) {
+function querySelectorAll(arg) {
   return document.querySelectorAll(arg);
 }
 
-// Load Component
-function loacom() {
-  qsa(".component").forEach(function(e) {
+function loadComponent() {
+  querySelectorAll(".component").forEach(function(e) {
     let xhr = new XMLHttpRequest();
     xhr.onload = function() {
       if (this.status == 200) {
-        e.innerHTML = xhr.responseText;
+        e.parentElement.innerHTML = xhr.responseText;
       }
     }
     xhr.open("GET", e.getAttribute("data-component"), true);
@@ -17,4 +15,35 @@ function loacom() {
   });
 }
 
-export { qsa, loacom }
+function random(vlr) {
+  return Math.floor(Math.random() * (vlr + 1))
+}
+
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = 'expires=' + d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+}
+
+function getCookie(cname) {
+  let name = cname + '=';
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
+
+export { querySelectorAll, loadComponent, random, randomBetween, setCookie, getCookie }
