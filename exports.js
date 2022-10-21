a99831451f1a3ff43c5a85b8c6f9a519a4132ff4
Oscar Sei Iti Taniguchi <tseiiti@gmail.com>
2022-10-21 16:03:24 -0300

    update exports.js

--- exports.js
+++ exports.js
@@ -1,2 +1,16 @@
-<h1>Hello, component!</h1>
-<p>teste de componente</p>
+function qsa(arg) {
+  return document.querySelectorAll(arg);
+}
+
+function loadComponents() {
+  qsa(".component").forEach(function(e) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      if (this.status == 200) {
+        e.innerHTML = xhr.responseText;
+      }
+    }
+    xhr.open("GET", e.getAttribute("data-component"), true);
+    xhr.send();
+  });
+}
