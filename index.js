531717974102ec96244c7422a5b2dd41ca403eb8
tseiiti termux <tseiiti@gmail.com>
2022-10-27 08:45:29 -0300

    colunas

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// import { pokes, querySelector, loadComponent, randomBetween } from './exports.js';
-
-
 function getImageUrlByName(pok_id){
   return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${ pok_id.toString().padStart(3, '0') }.png`;
 }
@@ -9,29 +6,10 @@ var pok_id = randomBetween(1, 905);
 
 function pokeImageLoad() {
   let img = querySelector('#poke_image');
-  if (!img.alt) {
-    img.src = getImageUrlByName(pok_id);
-    img.alt = 'Imagem selecionada';
-    console.log(pok_id);
-    console.log(pokes[pok_id]);
-    console.log(getImageUrlByName(pok_id));
-  }
+  if (img.alt) return false;
+  
+  img.src = getImageUrlByName(pok_id);
+  img.alt = 'Imagem selecionada';
 }
 
-loadComponent();
-  
-
-
-
-// let path = window.location.pathname;
-// let page = path.split('/').pop();
-// console.log( page );
-// console.log( page.split('.').pop() );
-
-// function getRootUrl(pok_name) {
-//   if (pokes.includes(pok_name)) {
-//     return 'https://www.pokemon.com/br/pokedex/' + pok_name;
-//   }
-// }
-
-// console.log(pokes.indexOf('venusaur'));
+loadComponent('index_component', 'index_component.html');
