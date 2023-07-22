f446f23dbeb1261dbb25119ab286ba1c49b1ecb6
Oscar Sei Iti Taniguchi <oscar.taniguchi@tecnogroup.com.br>
2023-07-22 12:26:15 -0300

    piano test 5

--- js/piano.js
+++ js/piano.js
@@ -1,3 +1,34 @@
+function handleDown(key) {
+  if (key.className.includes('black')) {
+    key.classList.add('black_pressed');
+    return
+  }
+  key.style.background = '#ddd';
+}
+
+function handleUp(key) {
+  if (key.className.includes('black')) {
+    key.classList.remove('black_pressed');
+    return
+  }
+  key.style.background = 'white';
+}
+
+onkeydown = function(event) {
+  handleDown(qs(`.key[data-key=${event.key}]`));
+}
+
+onkeyup = function(event) {
+  handleUp(qs(`.key[data-key=${event.key}]`));
+}
+
+qsa('.key').forEach(function(key) {
+  key.onmousedown = function() { handleDown(this); }
+  key.onmouseup = function() { handleUp(this); }
+  key.onmouseleave = function() { handleUp(this); }
+  key.ontouchstart = function() { handleDown(this); }
+  key.ontouchend = function() { handleUp(this); }
+});
 
 afterLoad(function() {
   // alert(1)
